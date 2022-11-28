c8746a23bd56360e80a9cacbbbfd397c2888228a
Matteo Robidoux <SDJ00008@gmail.com>
2022-11-28 18:54:24 -0500

    Added methods to pokemon and items class

--- app/src/main/java/com/example/pokemonapp/Pokemon.kt
+++ app/src/main/java/com/example/pokemonapp/Pokemon.kt
@@ -5,6 +5,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlin.math.floor
 import kotlin.math.pow
+import kotlin.math.roundToInt
 
 @Entity(tableName = "pokemon_table")
 class Pokemon(@PrimaryKey @ColumnInfo(name = "pokemonNumber") val pokemonNumber: String,
@@ -23,6 +24,7 @@ class Pokemon(@PrimaryKey @ColumnInfo(name = "pokemonNumber") val pokemonNumber:
 
     var name = species
     var level = floor(baseExperienceReward.toDouble().pow(1/3)).toInt()
+    var experience = level*level*level
     var baseStatAttack = pokemonBaseStateAttack * (50 + level)/50
     var baseStatDefense = pokemonBaseStatDefense * (50 + level)/50
     var baseStatSpecialAttack = pokemonBaseStatSpecialAttack * (50 + level)/50
@@ -40,4 +42,67 @@ class Pokemon(@PrimaryKey @ColumnInfo(name = "pokemonNumber") val pokemonNumber:
         }
     }
 
+    fun calculateExperienceGained(oppenentPokemon: Pokemon) : Boolean{
+        var newMoveAvailable = false
+        val prevLevel = level
+        experience = (0.3 * oppenentPokemon.baseExperienceReward * oppenentPokemon.level).roundToInt()
+        level = floor(baseExperienceReward.toDouble().pow(1/3)).toInt()
+        if(prevLevel != level){
+            increaseStats()
+             newMoveAvailable = newMoveAvailable()
+        }
+        return newMoveAvailable
+    }
+
+    private fun increaseStats(){
+        baseStatDefense = baseStatDefense * (50 + level)/50
+        baseStatSpecialAttack = baseStatSpecialAttack * (50 + level)/50
+        baseStatSpecialDefense = baseStatSpecialDefense * (50 + level)/50
+        baseStatSpeed = baseStatSpeed * (50 + level)/50
+        baseStatMaxHp = baseStatMaxHp * (50 + level)/50
+    }
+
+    private fun newMoveAvailable(): Boolean{
+        for(move in pokemonMoves){
+            if(moves.size < 4 && move.level_learned_at == level){
+                if(!moves.contains(move)) {
+                    moves.add(move)
+                    return false
+                }
+            } else if(moves.size == 4 && move.level_learned_at == level){
+                if(!moves.contains(move)) {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+    fun attackOpponent(opposingPokemon: Pokemon, move: Move) : Double{
+        if(move.damageClass == "physical") {
+            val calculaton = ((2 * level) / 5 + 2).toDouble()
+            var baseDamage =
+                (1 / 50 * calculaton * move.power * (baseStatAttack / opposingPokemon.baseStatDefense) + 2).toDouble()
+            for (type in types) {
+                if (move.type == type) {
+                    baseDamage *= 1.5
+                    break
+                }
+                //TODO Create the type chart to calculate effective not effective moves
+            }
+            return baseDamage
+        } else {
+            val calculaton = ((2 * level) / 5 + 2).toDouble()
+            var baseSpecialDamage =
+                (1 / 50 * calculaton * move.power * (baseStatSpecialAttack / opposingPokemon.baseStatSpecialDefense) + 2).toDouble()
+            for (type in types) {
+                if (move.type == type) {
+                    baseSpecialDamage *= 1.5
+                    break
+                }
+                //TODO Create the type chart to calculate effective not effective moves
+            }
+            return baseSpecialDamage
+        }
+    }
 }
--- app/src/main/java/com/example/pokemonapp/Trainer.kt
+++ app/src/main/java/com/example/pokemonapp/Trainer.kt
@@ -4,9 +4,10 @@ import androidx.room.Entity
 
 
 @Entity(tableName = "trainer_table")
-class Trainer(trainerName: String) {
+class Trainer(var trainerName: String) {
     lateinit var pokemonTeam: PokemonTeam
     lateinit var pokemonCollection: PokemonCollection
+    var items = Items()
 
     @JvmName("setPokemonTeam1")
     fun setPokemonTeam(team: PokemonTeam){
@@ -16,5 +17,13 @@ class Trainer(trainerName: String) {
     @JvmName("setPokemonCollection1")
     fun setPokemonCollection(collection: PokemonCollection){
         pokemonCollection = collection
+    }
+
+    fun addPokemon(pokemon: Pokemon){
+        if(pokemonTeam.pokemons.size < 6){
+            pokemonTeam.pokemons.add(pokemon)
+        } else {
+            pokemonCollection.pokemons.add(pokemon)
+        }
     }
 }
