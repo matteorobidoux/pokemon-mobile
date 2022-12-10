6bc4cfccbc5a5fac19a8c3c136bc695526536493
Veronika Pontolillo <veronika.pontolillo@dawsoncollege.qc.ca>
2022-12-10 15:56:10 -0500

    handled more merge conflicts

--- app/src/main/java/com/example/pokemonapp/objects/Items.kt
+++ app/src/main/java/com/example/pokemonapp/objects/Items.kt
@@ -15,13 +15,5 @@ class Items(@PrimaryKey @ColumnInfo(name="name") val name: String,
 
     }
 
-<<<<<<< HEAD
-    class Items() : java.io.Serializable {
-        @PrimaryKey(autoGenerate = true)
-        var pokeBalls = 0
-        var potions = 0;
-    }
 }
-=======
-}
->>>>>>> 5bca2d305b8bd869195873932bdb3d0db1591a18
+
